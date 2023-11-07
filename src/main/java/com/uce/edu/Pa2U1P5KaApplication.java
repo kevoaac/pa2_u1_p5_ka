bd44b5cfe3218a37d6441e55847c807c2754f2f1
Jhonjaxde <jjarteaga@uce.edu.ec>
2023-11-07 18:55:13 -0500

    Primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5KaApplication.java
@@ -0,0 +1,23 @@
+package com.uce.edu;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5KaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5KaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
+		System.out.println("Hola soy kevin Andrade");
+		System.out.println(new Error("Error nuevo"));
+		
+	}
+
+}
