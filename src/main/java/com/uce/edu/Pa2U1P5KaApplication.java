a553630e930d8d100bdb6afefd2a53f6d57ce1c3
Kevin Andrade <kevinandradecardenas@gmail.com>
2023-11-10 19:21:04 -0500

    taller4V2

--- src/main/java/com/uce/edu/Pa2U1P5KaApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5KaApplication.java
@@ -18,6 +18,7 @@ public class Pa2U1P5KaApplication implements CommandLineRunner{
 		System.out.println("Hola soy kevin Andrade");
 		System.out.println("Cambio en la misma rama main");
 		System.out.println("Cambio para rama taller4");
+		System.out.println("taller4V2");
 		
 	}
 
